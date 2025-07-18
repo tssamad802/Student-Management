8c431f19a8c24843698046ba6e5e8bae378cabd9
samad <iamsamad148@gmail.com>
2025-07-18 16:57:45 +0500

    set format my code

--- includes/contr.php
+++ includes/contr.php
@@ -1,18 +1,23 @@
 <?php
-class controller extends user {
-    public function is_empty_inputs($name, $email, $class) {
+class controller extends user
+{
+    public function is_empty_inputs($name, $email, $class)
+    {
         return empty($name) || empty($email) || empty($class);
     }
 
-    public function is_name_invalid($name) {
+    public function is_name_invalid($name)
+    {
         return !preg_match("/[a-zA-Z ]+$/", $name);
     }
 
-    public function is_email_invalid($email) {
+    public function is_email_invalid($email)
+    {
         return !filter_var($email, FILTER_VALIDATE_EMAIL);
     }
 
-    public function is_class_invalid($class) {
+    public function is_class_invalid($class)
+    {
         return !preg_match("/[a-zA-Z0-9]+$/", $class);
     }
 }
